cef3f0188592dd151cb476805df3bf9708809165
Juan Villota <villota99@hotmail.com>
2025-03-09 19:59:02 -0500

    Update index.html, main.jsx, and global.css: add star animation and adjust background color

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/global.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('main_content')).render(
   <StrictMode>
+    <div className="stars"></div> {/* Add stars container */}
     <App />
   </StrictMode>,
-)
+);
