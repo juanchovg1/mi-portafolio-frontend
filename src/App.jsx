81a79d8464011c9425ecca718239a28435b285cf
Juan Villota <villota99@hotmail.com>
2025-03-11 16:31:44 -0500

    Remove Header component from App for improved layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
 import ProfileSection from "./components/ProfileSection";
 import AboutMe from "./components/AboutMe";
 import ResumeSection from "./components/ResumeSection"; // Import ResumeSection
@@ -8,7 +7,6 @@ import ResumeSection from "./components/ResumeSection"; // Import ResumeSection
 function App() {
   return (
     <Router>
-      <Header />
       <ProfileSection />
       <AboutMe />
       <ResumeSection /> {/* Include ResumeSection */}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/global.css';
 import App from './App.jsx';
+import Header from './components/Header'; // Import Header
 
 createRoot(document.getElementById('main_content')).render(
   <StrictMode>
+    <Header /> {/* Render Header here */}
     <App />
   </StrictMode>,
 );
